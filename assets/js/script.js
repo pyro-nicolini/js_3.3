20dbec6cd92a2edcdc57d5337a55a2ca2571c88b
Piero Nicolini <naturalnen@gmail.com>
2025-01-15 00:05:46 -0300

    fix logics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-//este archivo se comparte en 2 páginas html.
+//Este archivo de JS lo comparto en pintar.html y 4_colores.html
 
-/* ejercicio pintar.html */
-const ele = document.getElementById("ele1")
-function pintar(elemento, color) {
-   elemento.style.backgroundColor = color;
+function pintar(elemento, color = "green") { // declaro la function con elemento, color y color por defecto en verde.
+        elemento.style.backgroundColor = color;
 }
 
-if (ele) { // valido sí existe en el html
-    ele.style.backgroundColor = "green";
-    ele.addEventListener("click", (e) => pintar(e.target, 'yellow'));
-}
+const ele = document.getElementById("ele1");
+
+if (ele) {
+    pintar(ele)
+    ele.addEventListener("click", (e) => pintar(e.target, 'yellow'))
+} 
 
 
 /* ejercicio 4_colores.html */ 
@@ -18,7 +18,8 @@ const color2 = document.getElementById("id2");
 const color3 = document.getElementById("id3");
 const color4 = document.getElementById("id4");
 
-if (color1 && color2 && color3 && color4) {  // valido sí existe en el html, para evitar errores y re-utilizó la función pintar
+
+if (color1 && color2 && color3 && color4) {  // valido sí existen mis elementos en el html, para evitar errores en mi página 1 y re-utilizó la función pintar
     color1.addEventListener("click", () => pintar(color1, "black"));
     color2.addEventListener("click", () => pintar(color2, "black"));
     color3.addEventListener("click", () => pintar(color3, "black"));
